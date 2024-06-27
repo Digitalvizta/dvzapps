2e9f9ccecc4a864f0ce8428cca1779ddfb0dae16
Muhammad Imran Sarwar <mmanisarwar@gmail.com>
2024-06-27 14:00:24 +0500

    Asset Management

---
+++ dvz_asset_management/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Asset Management',
+    'version': '16.0.1',
+    'category': 'custom',
+    'summary': 'Asset Management',
+    'license':'LGPL-3',
+    'price' : 20.0,
+    'currency': 'USD',
+    'description': """
+     Asset management is a simple system to manage assets owned by an organization.
+""",
+    'author' : 'DigitalVizta',
+    'website' : '',
+    'depends': ['base','mail'],
+    'data': [
+         'security/security.xml',
+         'security/ir.model.access.csv',
+         'views/asset_sequence.xml',
+         'views/asset_view.xml',
+         'views/asset_move_view.xml'
+             
+             ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
+
+
