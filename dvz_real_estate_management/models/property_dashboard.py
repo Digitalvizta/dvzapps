d9b679462104c69b0b9d3809d7873f899f54ebd2
Sami <samiullahch274@gmail.com>
2024-07-25 16:32:58 +0500

    real estate management

---
+++ dvz_real_estate_management/models/property_dashboard.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, models
+
+
+class PropertyDashboard(models.Model):
+    _name = 'property.dashboard'
+    _description = 'Property Dashboard'
+    
+    def get_details(self):
+        total_property = self.env['property.property'].search_count([])
+        sold_property = self.env['property.property'].search_count([('status', '=', 'sold')])
+        print("==============>", total_property)
+        return {
+            'total_property': total_property,
+        }
